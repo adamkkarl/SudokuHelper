5c78c55f9683bd5e9baf00bc5c7b4c9bfcde0e24
adamkkarl <adamkkarl@gmail.com>
2022-08-05 15:30:08 -0400

    initialized main file

---
+++ sudokuhelper.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+"""Experiments at datermining optimal wordle strategy"""
+
+__author__ = "Adam Karl"
+
+def sudokuSolver(grid):
+    """Given a 9x9 sudoku grid with some numbers filled, solve the puzzle"""
+    
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
+        
